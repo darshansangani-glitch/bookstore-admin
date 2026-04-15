296b664a3c716caca525abeb436c3fbdd824b5ce
harsh_joshi0 <joshiharsh617@gmail.com>
2026-04-15 15:00:05 +0530

    Remove Material UI in Login and Fix it

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,35 +1,19 @@
-// import { useEffect, useState } from "react";
-// import darkLogo from "../assets/darkLogo.png";
-// import { MdOutlineMailOutline } from "react-icons/md";
-// import { RiLockPasswordLine } from "react-icons/ri";
-// import { useNavigate } from "react-router-dom";
-import React, { useEffect, useState } from "react";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import FormControl from "@mui/material/FormControl";
-import IconButton from "@mui/material/IconButton";
-import InputAdornment from "@mui/material/InputAdornment";
-import InputLabel from "@mui/material/InputLabel";
-import TextField from "@mui/material/TextField";
-import Typography from "@mui/material/Typography";
-import Visibility from "@mui/icons-material/Visibility";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
-import Input from "@mui/material/Input";
+import {  useState } from "react";
+import { IoMailSharp } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
 import LightLogo from "../assets/lightLogo.png";
 import { login } from "../redux/features/slice/authSlice";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import { FaLock } from "react-icons/fa";
 
 export default function Login() {
-  const [showPassword, setShowPassword] = React.useState(false);
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const dispatch = useAppDispatch()
   const navigate = useNavigate();
 
-  const userToken = useAppSelector(s=>s.auth.token)
+  const userToken = useAppSelector(s => s.auth.token)
 
   const handleLogin = async () => {
     const url = `${import.meta.env.VITE_API_URL}/user/login`;
@@ -53,92 +37,69 @@ export default function Login() {
     }
   };
 
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
-
-  const handleMouseDownPassword = (
-    event: React.MouseEvent<HTMLButtonElement>,
-  ) => {
-    event.preventDefault();
-  };
-
-  const handleMouseUpPassword = (
-    event: React.MouseEvent<HTMLButtonElement>,
-  ) => {
-    event.preventDefault();
-  };
-
   return (
-    <div className=" flex w-250! h-130 justify-center mt-15 items-center border  ml-auto mr-auto  rounded-2xl">
-      <Box
-        className="flex flex-col w-[35%]! justify-center ml-auto  h-full!"
-        component="form"
-        sx={{ "& > :not(style)": { m: 1.5 } }}
-        noValidate
-        autoComplete="off"
-      >
-        <Typography className="align-center font-[Poppins] text-gray-700 font-extralight " variant="h3">
-          Welcome Back Admin!!
-        </Typography>
-        <TextField
-          id="email"
-          label="Email Address"
-          variant="standard"
-          name={email}
-          className="w-90 bg-transparent! text-black!"
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <FormControl sx={{ m: 1, width: "400px" }} variant="standard">
-          <InputLabel className="text-3" htmlFor="standard-adornment-password">
-            Password
-          </InputLabel>
-          <Input
-            name={password}
-            id="standard-adornment-password"
-            type={showPassword ? "text" : "password"}
-            onChange={(e) => setPassword(e.target.value)}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label={
-                    showPassword ? "hide the password" : "display the password"
-                  }
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  onMouseUp={handleMouseUpPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-          />
-        </FormControl>
+    <div className="w-full! flex items-center h-screen">
 
-        <Button
-          className="mt-7! w-90"
-          variant="contained"
+    <div className=" flex w-250! h-130 justify-center  items-center border  ml-auto mr-auto  rounded-2xl">
+      <form
+        className="flex flex-col w-[35%]! justify-center ml-auto m-1.5 gap-3  h-full!"
+        noValidate
+        autoComplete="none"
+
+      >
+        <p className="align-center font-[Poppins] text-gray-400 text-5xl leading-13 font-semibold ">
+          Welcome Back Admin!!
+        </p>
+        <label className="flex flex-col gap-1">
+          <span className="text-[18px] font-semibold">Email Address</span>
+          <div className="flex gap-1 items-center border p-2 rounded-xl border-slate-400">
+            <IoMailSharp className="text-xl text-slate-400 " />
+            <input
+              id="email"
+              name={email}
+              placeholder="Enter Your Email..."
+              className="w-full p-1! text-[18px]  bg-white! text-black! focus:outline-0! cursor-pointer"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+        </label>
+        <label className="flex flex-col gap-1" htmlFor="standard-adornment-password">
+          <span className="text-[18px] font-semibold">Password</span>
+          <div className="flex gap-1 items-center rounded-xl p-2 border border-slate-400">
+            <FaLock className="text-xl text-slate-400"/>
+            <input
+              name={password}
+              className="p-1 w-full text-[18px] text-black bg-white focus:outline-0! cursor-pointer"
+              placeholder="Enter your Password..."
+              id="standard-adornment-password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+        </label>
+
+        <button
+          className="mt-7! w-full border-0 font-bold text-xl hover:bg-green-600 p-3 rounded-2xl bg-green-400 text-white cursor-pointer"
           type="button"
           onClick={handleLogin}
         >
           Log In
-        </Button>
-      </Box>
+        </button>
+      </form>
       <div className="bg-black p-25 w-[50%] mr-0 ml-auto h-full justify-center items-center rounded-br-2xl rounded-tr-2xl flex flex-col gap-5">
-        <img className="w-18 h-16" src={LightLogo} alt="Light Logo" />
-        <Typography
-          variant="h2"
-          className="flex  items-center text-white font-black"
+        <img className="w-25 h-25" src={LightLogo} alt="Light Logo" />
+        <p
+          className="flex  items-center text-white font-black text-5xl"
         >
           <span className="text-green-500 flex!">Book</span>Warm
-        </Typography>
-        <Typography
-          variant="h4"
-          className="flex flex-col justify-center items-center text-white"
+        </p>
+        <p
+          className="flex flex-col text-3xl  justify-center items-center text-white"
         >
           Admin Login Page{" "}
-        </Typography>
+        </p>
       </div>
+    </div>
     </div>
   );
 }
